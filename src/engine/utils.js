407ee0e6c791ecf803437b22a89c4bdab9f81831
Daniel Sharkov <scharktv@gmail.com>
2019-06-30 18:47:21 +0200

    Setup Github Pages

--- src/engine/utils.js
+++
@@ -1,6 +0,0 @@
-export function UUID() {
-	return 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, c => {
-		let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8)
-		return v.toString(16)
-	})
-}
